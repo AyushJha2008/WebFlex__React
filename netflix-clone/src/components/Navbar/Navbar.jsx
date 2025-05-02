c929baf9a067e986560e239c987729918305b952
AyushJha2008 <ayushsjha2008@gmail.com>
2025-05-02 23:39:44 +0530

    styled navbar

--- netflix-clone/src/components/Navbar/Navbar.jsx
+++ netflix-clone/src/components/Navbar/Navbar.jsx
@@ -8,16 +8,15 @@ import drop_icon from '../../assets/drop.png'
 
 const Navbar = () => {
   return (
-    <div className='Navpar'>
+    <div className='navbar'>
         <div className="navbar-left">
-          <img src={logo} alt="" />
+          <img src={logo} />
           <ul>
             <li>Home</li>
             <li>TV Shows</li>
             <li>Movies</li>
             <li>New & Popular</li>
             <li>Browse by Language</li>
-            <li>Home</li>
           </ul>
         </div>
         <div className="navbar-right">
@@ -26,7 +25,10 @@ const Navbar = () => {
           <img src={bell_icon} className='icon' />
           <div className='navbar-profile'>
             <img src={prof_icon} className='profile' />
-            <img src={drop_icon} />
+            {/* <img src={drop_icon} /> */}
+            <div className="dropdown">
+              <p>Sign out</p>
+            </div>
           </div>
         </div>
     </div>
