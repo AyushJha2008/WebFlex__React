a3138156e32114401f50538319e2f8c2e9e46e93
AyushJha2008 <ayushsjha2008@gmail.com>
2025-05-05 00:49:49 +0530

    created title card section

--- WebFlex/src/components/TitleCards/TitleCards.jsx
+++ WebFlex/src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/CardData'
 
 const TitleCards = () => {
   return (
-    <div className='TitleCards'>
-
+    <div className='titleCards'>
+      <h2>Popular on WebFlex</h2>
+      <div className="cardList">
+        {cards_data.map((card, index)=>{
+          return <div className="card" key={index}>
+            <img src={card.image} />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
--- WebFlex/src/pages/Home/Home.jsx
+++ WebFlex/src/pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import heroBanner from '../../assets/heroBanner.png'
 import heroTitle from '../../assets/hero-Title.png'
+import playIcon from '../../assets/Play-icon.png'
+import infoIcon from '../../assets/info-icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards'
 
 const Home = () => {
   return (
@@ -10,11 +13,17 @@ const Home = () => {
       <Navbar/>
       <div className='hero'>
         <img src={heroBanner} className='banner-img' />
+
         <div className='hero-caption'>
           <img src={heroTitle} className='caption-img' />
           <p>Monkey D. Luffy and his pirate crew explore a fantastical world of endless oceans and exotic islands in search of the world's ultimate treasure to become the next Pirate King.</p>
+          <div className="hero-btns">
+            <button className='btn'><img src={playIcon} />Play</button>
+            <button className='btn dark-btn'><img src={infoIcon} />More Info</button>
+          </div>
         </div>
       </div>
+      <TitleCards/>
     </div>
   )
 }
