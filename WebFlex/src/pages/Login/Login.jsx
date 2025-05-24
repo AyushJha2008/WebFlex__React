83f9eb17a17b312b8c4f8d8c51150ecea7cb3766
AyushJha2008 <ayushsjha2008@gmail.com>
2025-05-24 16:47:09 +0530

    created login page

--- WebFlex/src/pages/Login/Login.jsx
+++ WebFlex/src/pages/Login/Login.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './Login.css'
+import logo from '../../assets/banner.png'
 
 const Login = () => {
   return (
     <div className='login'>
-
+      <img src={logo} />
+      <div className="logForm">
+        <h2>Welcome to WebFlex</h2>
+        <form>
+          <input type="text" placeholder='Your Name'/>
+          <input type="email" placeholder='Your Email' />
+          <input type="password" placeholder='Password' />
+          <button>Sign Up</button>
+          <div className='formHelp'>
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help</p>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
